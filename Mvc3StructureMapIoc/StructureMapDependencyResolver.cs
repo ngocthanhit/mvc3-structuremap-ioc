a205e3cc7ddbb246a07bda30c75dc2b9ed73989e
Eric Sowell <eric.sowell@gmail.com>
2011-01-20 21:31:15 -0600

    first code push

---
+++ Mvc3StructureMapIoc/StructureMapDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using StructureMap;
+
+namespace Mvc3StructureMapIoc
+{
+    public class StructureMapDependencyResolver : IDependencyResolver
+    {
+        public StructureMapDependencyResolver(IContainer container)
+        {
+            _container = container;
+        }
+
+        private IContainer _container;
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
